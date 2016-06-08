be5f236961959e2f64a1e48deb9b179fea4c7be2
dhcrain <dhcrain@gmail.com>
2016-06-08 13:36:54 -0400

    Homework changed, abandon ship

--- wiring_up/views.py
+++ wiring_up/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from wiring_up.models import BaseballStat
 # Create your views here.
 
 
 def bbstat_view(request):
-    return HttpResponse("Hello World! stats?")
+    return HttpResponse(BaseballStat)
 
